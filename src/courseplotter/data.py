e7189a26ec36a3373d83594a9327b0938085a2c3
adamj <a.j.jones@sky.com>
2025-05-12 18:19:38 +0100

    added first iteration of working plotting class

--- src/courseplotter/data.py
+++ src/courseplotter/data.py
@@ -1,58 +1,113 @@
 import numba as nb
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+def check_columns(df: pd.DataFrame, columns: list[str]) -> None:
+    for column in columns:
+        if column not in df.columns:
+            raise ValueError(f"Column name `{column}` is required")
 
 
 @nb.njit
 def interpolate_times(times: np.ndarray, course: np.ndarray, frequency_s: int) -> np.ndarray:
     """
     Interpolate positions on the course for a single competitor.
-    - times: np.ndarray with columns [distance, time_s] for a single competitor.
-    - course: np.ndarray with columns [course_dist, course_lon, course_lat].
-    - frequency_s: The desired time frequency in seconds for the output.
+
+    Args:
+        times (np.ndarray): Columns [distance, time] for a single competitor
+        course (np.ndarray): Columns [distance, longitude, latitude]
+        frequency_s (int): Frequency for interpolation in seconds
+
+    Returns:
+        np.ndarray: Interpolated positions of a competitor at the provided interval [seconds, distance, longitude, latitude]
     """
+    out_size = (0, 4)
+
     if times.shape[0] == 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     if times[0, 0] != 0.0:
         zero_start_record = np.zeros((1, times.shape[1]), dtype=times.dtype)
         times = np.vstack((zero_start_record, times))
 
     if course.shape[0] == 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     r_dist = times[:, 0]
     r_secs = times[:, 1]
     c_dist = course[:, 0]
 
     if r_dist.shape[0] == 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     c_secs = np.interp(c_dist, r_dist, r_secs)
 
     if times.shape[0] == 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     max_time = times[-1, 1]
     if frequency_s <= 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     p_secs = np.arange(0.0, max_time + frequency_s, float(frequency_s))
 
     if p_secs.shape[0] == 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     c_lon = course[:, 1]
     c_lat = course[:, 2]
 
     if c_secs.shape[0] == 0:
-        return np.empty((0, 3), dtype=np.float64)
+        return np.empty(out_size, dtype=np.float64)
 
     p_lon = np.interp(p_secs, c_secs, c_lon)
     p_lat = np.interp(p_secs, c_secs, c_lat)
+    p_dist = np.interp(p_secs, c_secs, c_dist)
 
-    out_data = np.empty((p_secs.shape[0], 3), dtype=np.float64)
+    out_data = np.empty((p_secs.shape[0], 4), dtype=np.float64)
     out_data[:, 0] = p_secs
-    out_data[:, 1] = p_lon
-    out_data[:, 2] = p_lat
+    out_data[:, 1] = p_dist
+    out_data[:, 2] = p_lon
+    out_data[:, 3] = p_lat
 
     return out_data
+
+
+def apply_interpolation(
+    group: pd.DataFrame,
+    course_arr: np.ndarray,
+    frequency_s: int,
+    pbar: tqdm,
+) -> np.ndarray:
+    """
+    Wrapper function to apply the interpolation logic to a group (DataFrame for one runner).
+    """
+    arr = group[["distance", "time"]].values
+    out_cols = ["time", "distance", "longitude", "latitude"]
+
+    if arr.shape[0] == 0:
+        pbar.update()
+        return pd.DataFrame(np.empty((0, 3)), columns=out_cols)
+
+    df = pd.DataFrame(interpolate_times(arr, course_arr, frequency_s), columns=out_cols)
+    pbar.update()
+
+    return df
+
+
+def calculate_plotting_coordinates(timings: pd.DataFrame, course: pd.DataFrame, frequency: int) -> pd.DataFrame:
+    check_columns(timings, ["id", "distance", "time"])
+    check_columns(course, ["distance", "longitude", "latitude"])
+
+    course_array = course[["distance", "longitude", "latitude"]].values
+
+    with tqdm(total=len(timings["id"].unique()), position=0) as pbar:
+        interpolated_timings: pd.DataFrame = timings.groupby("id").apply(
+            apply_interpolation, course_arr=course_array, frequency_s=frequency, pbar=pbar, include_groups=False
+        )
+
+    interpolated_timings = interpolated_timings.dropna()
+
+    return interpolated_timings
---
+++ src/courseplotter/plotting.py
@@ -0,0 +1,187 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import contextily as cx
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from src.courseplotter.data import calculate_plotting_coordinates
+
+
+def get_randomness_size(distances: pd.Series, bins: int = 100, scale: int = 2500) -> np.ndarray:
+    density_values, bin_edges = np.histogram(distances, bins=bins, density=False)
+    density_values[density_values > scale] = scale
+
+    bin_indices = np.digitize(distances, bin_edges) - 1
+    bin_indices[distances < bin_edges[0]] = -1
+    bin_indices[distances >= bin_edges[-1]] = bins - 1
+
+    return density_values[bin_indices] / scale
+
+
+@dataclass
+class Theme:
+    plot_scale: int = 1
+    plot_width: int = 16
+    plot_height: int = 9
+    plot_zoom: float = 10
+    background_colour: str = "black"
+    font_colour: str = "white"
+
+
+class CoursePlotter:
+    def __init__(self, timings: pd.DataFrame, course: pd.DataFrame, theme: Theme) -> None:
+        self.timings = timings
+        self.course = course
+        self.positions = None
+
+        # Plot componenets
+        self.theme = theme
+        self.fig = None
+        self.ax = None
+        self.timer_text = None
+        self.course_plot = None
+        self.colour_scatter = None
+        self.white_scatter = None
+
+    def calculate_positions(self, frequency_s: int) -> None:
+        self.positions = calculate_plotting_coordinates(timings=self.timings, course=self.course, frequency=frequency_s)
+
+    def create_figure(self) -> None:
+        if self.positions is None:
+            raise ValueError("Positions must be calculated first: Run `CoursePlotter.calculate_positions`")
+
+        self.fig, self.ax = plt.subplots(figsize=(self.theme.plot_width, self.theme.plot_height))
+        self.ax.grid(False)
+        self.ax.axis("off")
+        self.ax.set_aspect("equal", "box")
+
+        # Apply configuration:
+        self._set_figure_colours()
+        self._set_axis_limits()
+        self._add_basemap()
+        self._add_timer_box()
+
+    def create_plots(self) -> None:
+        self.course_plot = self.ax.plot(
+            self.course["longitude"], self.course["latitude"], alpha=0.5, c="grey", zorder=1
+        )
+        self.colour_scatter = self.ax.scatter(
+            [], [], s=5, alpha=0.05, c=[], cmap="RdYlBu", vmin=0, vmax=42200, zorder=2
+        )
+        self.white_scatter = self.ax.scatter([], [], s=5, alpha=0.025, c="white", zorder=3)
+
+    def plot_data(self, time_s: Optional[int] = None) -> plt.Figure:
+        all = self.positions
+        df = all.loc[all["time"] == time_s].copy()
+
+        df["weights"] = get_randomness_size(df["distance"], 150, 1500) * 0.002
+        random_x = np.random.normal(scale=df["weights"], size=len(df))
+        random_y = np.random.normal(scale=df["weights"], size=len(df))
+
+        # Coloured dots:
+        df["longitude_noise"] = df["longitude"] + random_x
+        df["latitude_noise"] = df["latitude"] + random_y
+        coordinates = df[["longitude_noise", "latitude_noise"]].values
+        self.colour_scatter.set_offsets(coordinates)
+        self.colour_scatter.set_array(df["distance"])
+
+        # White dots:
+        df["longitude_noise"] = df["longitude"] + (random_x * 0.75)
+        df["latitude_noise"] = df["latitude"] + (random_y * 0.75)
+        coordinates = df[["longitude_noise", "latitude_noise"]].values
+        self.white_scatter.set_offsets(coordinates)
+
+        # Update timer:
+        hours = int(time_s // 3600)
+        minutes = int((time_s % 3600) // 60)
+        self.timer_text.set_text(f"{hours:02d}:{minutes:02d}")
+
+        return self.fig
+
+    def animate_plot(self, filename: str) -> None:
+        frames = self.positions["time"].unique()
+        ani = animation.FuncAnimation(
+            fig=self.fig,
+            func=self.update_animation,
+            frames=frames,
+            interval=400,  # milliseconds between frames (e.g., 200ms)
+            blit=True,  # Set to False if updating size/color per frame causes issues
+            repeat=True,
+        )
+
+        with tqdm(total=len(frames), desc="Saving video") as pbar:
+
+            def update_func(_i, _n) -> None:
+                pbar.update()
+
+            ani.save(filename, fps=24, dpi=72, progress_callback=update_func)
+
+    def update_animation(self, frame_seconds: int) -> None:
+        """
+        Updates the scatter plot for each frame (each unique second).
+
+        Args:
+            frame_seconds: The current 'time' value for this frame.
+
+        Returns:
+            A tuple containing the updated scatter plot PathCollection.
+        """
+        _ = self.plot_data(frame_seconds)
+
+        return (self.colour_scatter, self.white_scatter)
+
+    def _add_basemap(self) -> None:
+        try:
+            cx.add_basemap(self.ax, crs="EPSG:4326", source=cx.providers.CartoDB.DarkMatter)
+        except Exception as e:
+            raise Exception(
+                f"Could not add basemap: {e}\nEnsure you have internet access and a basemap source is available."
+            )
+
+    def _set_figure_colours(self) -> None:
+        self.fig.patch.set_facecolor(self.theme.background_colour)
+        self.ax.set_facecolor(self.theme.background_colour)
+
+    def _set_axis_limits(self) -> None:
+        # Calculate the range of the course data in both dimensions
+        lon_max = self.course["longitude"].max()
+        lon_min = self.course["longitude"].min()
+        lat_max = self.course["latitude"].max()
+        lat_min = self.course["latitude"].min()
+        lon_range = lon_max - lon_min
+        lat_range = lat_max - lat_min
+        max_range = max(lon_range, lat_range)
+
+        # Calculate the center of the data's bounding box
+        center_lon = (lon_min + lon_max) / 2
+        center_lat = (lat_min + lat_max) / 2
+
+        # Calculate the new limits based on the center and the max_range, adding the margin
+        ratio = self.theme.plot_width / self.theme.plot_height
+        margin = 0.01
+
+        new_lon_min = center_lon - (max_range / 2) - margin
+        new_lon_max = center_lon + (max_range / 2) + margin
+        new_lat_min = center_lat - ((max_range / ratio) / 2) - margin
+        new_lat_max = center_lat + ((max_range / ratio) / 2) + margin
+
+        # Set the plot limits to create a square area in data coordinates
+        self.ax.set_xlim(new_lon_min, new_lon_max)
+        self.ax.set_ylim(new_lat_min, new_lat_max)
+
+    def _add_timer_box(self) -> None:
+        self.timer_text = self.ax.text(
+            0.99,
+            0.01,
+            "",
+            horizontalalignment="right",
+            verticalalignment="bottom",
+            transform=self.ax.transAxes,  # This makes the coordinates relative to the axes
+            color=self.theme.font_colour,  # Set color for visibility
+            fontsize=24,
+            bbox=dict(boxstyle="round,pad=0.5", fc=self.theme.background_colour, alpha=0.5),
+        )
